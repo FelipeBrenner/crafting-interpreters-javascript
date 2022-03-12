b8ec8f93b706ba205ab38d4d5cac918b9dbe42fe
felipe ricardi <felipericardi5@gmail.com>
2022-03-12 01:23:05 -0300

    feat: creating object to contain token informations :sparkles:

---
+++ Token.js
@@ -0,0 +1,8 @@
+export class Token {
+    constructor(type, operator, codeLine, value) {
+        this.type = type;
+        this.operator = operator;
+        this.codeLine = codeLine;
+        this.value = value;
+    }
+}
