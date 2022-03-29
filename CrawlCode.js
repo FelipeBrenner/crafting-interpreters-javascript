00137e564a6ec6d2a17cf378317e4d9dde1829d6
FelipeBrenner <felipeobrenner@hotmail.com>
2022-03-28 23:18:03 -0300

    finished handle number

--- CrawlCode.js
+++ CrawlCode.js
@@ -2,176 +2,173 @@ import { Token } from "./Token.js";
 import { TokenEnum } from "./TokenEnum.js";
 
 export class CrawlCode {
-    constructor(code) {
-        this.tokens = [];
-        this.currentCharIndex = 0;
-        this.startIndex = 0;
-        this.line = 1;
-        this.code = code;
-    }
+  constructor(code) {
+    this.tokens = [];
+    this.currentCharIndex = 0;
+    this.startIndex = 0;
+    this.line = 1;
+    this.code = code;
+  }
 
-    crawl() {
-        return new Promise((resolve) => {
-            while (!this.isEndOfExpression()) {
-                this.startIndex = this.currentCharIndex;
-                this.scanForTokens();
-            }
-            resolve();
-        });
-    }
-
-    scanForTokens() {
-        const currentChar = this.nextCharacter();
-        switch (currentChar) {
-            case TokenEnum.OPEN_PAREN.value:
-                this.addToken(TokenEnum.OPEN_PAREN);
-                break;
-            case TokenEnum.CLOSE_PAREN.value:
-                this.addToken(TokenEnum.CLOSE_PAREN);
-                break;
-            case TokenEnum.SUBTRACT.value:
-                this.addToken(TokenEnum.SUBTRACT);
-                break;
-            case TokenEnum.PLUS.value:
-                this.addToken(TokenEnum.PLUS);
-                break;
-            case TokenEnum.MULTIPLY.value:
-                this.addToken(TokenEnum.MULTIPLY);
-                break;
-            case TokenEnum.DIVIDE.value:
-                this.addToken(TokenEnum.DIVIDE);
-                break;
-            case TokenEnum.ASSIGN.value:
-                this.addToken(
-                    this.matchNext(TokenEnum.EQUAL.expected)
-                        ? TokenEnum.EQUAL
-                        : TokenEnum.ASSIGN
-                );
-                break;
-            case TokenEnum.NOT.value:
-                this.addToken(
-                    this.matchNext(TokenEnum.NOT_EQUAL.expected)
-                        ? TokenEnum.NOT_EQUAL
-                        : TokenEnum.NOT
-                );
-                break;
-            case TokenEnum.LESS_THAN.value:
-                this.addToken(
-                    this.matchNext(TokenEnum.LESS_THAN_OR_EQUAL.expected)
-                        ? TokenEnum.LESS_THAN_OR_EQUAL
-                        : TokenEnum.LESS_THAN
-                );
-                break;
-            case TokenEnum.GREATER_THAN.value:
-                this.addToken(
-                    this.matchNext(TokenEnum.GREATER_THAN_OR_EQUAL.expected)
-                        ? TokenEnum.GREATER_THAN_OR_EQUAL
-                        : TokenEnum.GREATER_THAN
-                );
-                break;
-            case TokenEnum.STRING_DELIMITER.value:
-                this.handleString();
-                break;
-            case " ":
-            case "\r":
-            case "\t":
-                // Ignore whitespace.
-                break;
-            case "\n":
-                this.line++;
-                break;
-            default:
-                if(this.isDigit(currentChar)){
-                    this.handleNumber();
-                }else{
-                    console.error(currentChar + " - Unknown token");
-                }               
-                break;
-        }
-
-        return this.tokens;
-    }
-
-    isDigit(digit){
-        return digit >= '0' && digit <= '9';
-    }
-
-    handleString() {
-        while (
-            this.getCharAtCurrent() != TokenEnum.STRING_DELIMITER.value &&
-            !this.isEndOfExpression()
-        ) {
-            if (this.getCharAtCurrent() == TokenEnum.END_OF_LINE.value) {
-                this.line++;
-            }
-            this.currentCharIndex++;
-        }
-
-        if (this.isEndOfExpression()) {
-            console.error("String not finished till end of code");
-            return;
-        }
-
-        this.currentCharIndex++;
-
-        const value = this.code.substring(
-            this.startIndex + 1,
-            this.currentCharIndex - 1
-        );
-        this.addToken(TokenEnum.STRING_DELIMITER, value);
-    }
-
-    getCharAtCurrent() {
-        return this.code[this.currentCharIndex];
-    }
-
-    matchRegex(expression) {
-        let regExp = new RegExp(expression);
-        return regExp.exec(this.getCharAtCurrent()) != null;
-    }
-
-    matchNext(expected) {
-        if (
-            this.isEndOfExpression() ||
-            !this.matchRegex(expected)
-        ) {
-            return false;
-        }
-
-        this.currentCharIndex++;
-        return true;
-    }
-
-    nextCharacter() {
-        return this.code.charAt(this.currentCharIndex++);
-    }
-
-    addToken(tokenEnum, value) {
-        const text = this.code.substring(this.startIndex, this.currentCharIndex);
-        this.tokens.push(new Token(tokenEnum, text, this.line, value));
-    }
-
-    isEndOfExpression() {
-        return this.currentCharIndex >= this.code.length;
-    }
-
-    handleNumber() {
+  crawl() {
+    return new Promise((resolve) => {
+      while (!this.isEndOfExpression()) {
         this.startIndex = this.currentCharIndex;
-        while (this.isDigit(this.nextCharacter())) 
-            
+        this.scanForTokens();
+      }
+      resolve();
+    });
+  }
 
-        // Look for a fractional part.
-        if (this.nextCharacter() == TokenEnum.FLOAT_DELIMITER && this.isDigit(this.matchNext("[0-9]"))) {
-            
-            while (this.isDigit(this.nextCharacter()));
-        }
-
-        
-        const value = this.code.substring(
-            this.startIndex + 1,
-            --this.currentCharIndex
+  scanForTokens() {
+    const currentChar = this.nextCharacter();
+    switch (currentChar) {
+      case TokenEnum.OPEN_PAREN.value:
+        this.addToken(TokenEnum.OPEN_PAREN);
+        break;
+      case TokenEnum.CLOSE_PAREN.value:
+        this.addToken(TokenEnum.CLOSE_PAREN);
+        break;
+      case TokenEnum.SUBTRACT.value:
+        this.addToken(TokenEnum.SUBTRACT);
+        break;
+      case TokenEnum.PLUS.value:
+        this.addToken(TokenEnum.PLUS);
+        break;
+      case TokenEnum.MULTIPLY.value:
+        this.addToken(TokenEnum.MULTIPLY);
+        break;
+      case TokenEnum.DIVIDE.value:
+        this.addToken(TokenEnum.DIVIDE);
+        break;
+      case TokenEnum.ASSIGN.value:
+        this.addToken(
+          this.matchNext(TokenEnum.EQUAL.expected)
+            ? TokenEnum.EQUAL
+            : TokenEnum.ASSIGN
         );
-        console.log(value);
-        this.addToken(TokenEnum.NUMBER, parseFloat(value));
+        break;
+      case TokenEnum.NOT.value:
+        this.addToken(
+          this.matchNext(TokenEnum.NOT_EQUAL.expected)
+            ? TokenEnum.NOT_EQUAL
+            : TokenEnum.NOT
+        );
+        break;
+      case TokenEnum.LESS_THAN.value:
+        this.addToken(
+          this.matchNext(TokenEnum.LESS_THAN_OR_EQUAL.expected)
+            ? TokenEnum.LESS_THAN_OR_EQUAL
+            : TokenEnum.LESS_THAN
+        );
+        break;
+      case TokenEnum.GREATER_THAN.value:
+        this.addToken(
+          this.matchNext(TokenEnum.GREATER_THAN_OR_EQUAL.expected)
+            ? TokenEnum.GREATER_THAN_OR_EQUAL
+            : TokenEnum.GREATER_THAN
+        );
+        break;
+      case TokenEnum.STRING_DELIMITER.value:
+        this.handleString();
+        break;
+      case " ":
+      case "\r":
+      case "\t":
+        // Ignore whitespace.
+        break;
+      case "\n":
+        this.line++;
+        break;
+      default:
+        if (this.isDigit(currentChar)) {
+          this.handleNumber();
+        } else {
+          console.error(currentChar + " - Unknown token");
+        }
+        break;
     }
+
+    return this.tokens;
+  }
+
+  isDigit(digit) {
+    return digit >= "0" && digit <= "9";
+  }
+
+  handleString() {
+    while (
+      this.getCharAtCurrent() != TokenEnum.STRING_DELIMITER.value &&
+      !this.isEndOfExpression()
+    ) {
+      if (this.getCharAtCurrent() == TokenEnum.END_OF_LINE.value) {
+        this.line++;
+      }
+      this.currentCharIndex++;
+    }
+
+    if (this.isEndOfExpression()) {
+      console.error("String not finished till end of code");
+      return;
+    }
+
+    this.currentCharIndex++;
+
+    const value = this.code.substring(
+      this.startIndex + 1,
+      this.currentCharIndex - 1
+    );
+    this.addToken(TokenEnum.STRING_DELIMITER, value);
+  }
+
+  getCharAtCurrent() {
+    return this.code[this.currentCharIndex];
+  }
+
+  matchRegex(expression) {
+    let regExp = new RegExp(expression);
+    return regExp.exec(this.getCharAtCurrent()) != null;
+  }
+
+  matchNext(expected) {
+    if (this.isEndOfExpression() || !this.matchRegex(expected)) {
+      return false;
+    }
+
+    this.currentCharIndex++;
+    return true;
+  }
+
+  nextCharacter() {
+    return this.code.charAt(this.currentCharIndex++);
+  }
+
+  addToken(tokenEnum, value) {
+    const text = this.code.substring(this.startIndex, this.currentCharIndex);
+    this.tokens.push(new Token(tokenEnum, text, this.line, value));
+  }
+
+  isEndOfExpression() {
+    return this.currentCharIndex >= this.code.length;
+  }
+
+  handleNumber() {
+    while (this.isDigit(this.getCharAtCurrent())) {
+      this.nextCharacter();
+    }
+
+    if (
+      this.getCharAtCurrent() == TokenEnum.FLOAT_DELIMITER.value &&
+      this.isDigit(this.matchNext("[0-9]"))
+    ) {
+      this.nextCharacter();
+      while (this.isDigit(this.getCharAtCurrent())) {
+        this.nextCharacter();
+      }
+    }
+
+    const value = this.code.substring(this.startIndex, this.currentCharIndex);
+
+    this.addToken(TokenEnum.NUMBER, parseFloat(value));
+  }
 }
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import { CrawlCode } from "./CrawlCode.js";
 
-const testExpression = "5/3!=2'teste'\n4+5";
+const testExpression = "52.9/3!=2'teste'\n4+5";
 
 const crawlCode = new CrawlCode(testExpression);
 crawlCode.crawl().then(() => {
-    console.log(crawlCode.tokens);
+  console.log(crawlCode.tokens);
 });
