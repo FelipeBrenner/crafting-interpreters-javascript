8a532be3a2b16d79de58e5295546959368bf42b1
felipe ricardi <felipericardi5@gmail.com>
2022-04-16 11:33:55 -0300

    feat: adding binary evaluation for relational cases

--- EvaluateTree.js
+++ EvaluateTree.js
@@ -15,17 +15,39 @@ export class EvaluateTree {
             case operation instanceof TreeExpr.Literal:
                 return operation.value;
             case operation instanceof TreeExpr.Unary:
-                return this.unaryEvaluation(operation.operator, operation.right);
+                return this.unaryEvaluation(operation);
+            case operation instanceof TreeExpr.Binary:
+                return this.binaryEvaluation(operation);
         }
     }
 
-    unaryEvaluation(operator, rightHand) {
-        const rightHandValue = this.evaluate(rightHand);
+    binaryEvaluation(operation) {
+        const leftHandValue = this.evaluate(operation.left);
+        const rightHandValue = this.evaluate(operation.right);
 
-        switch(operator) {
-            case TokenEnum.NOT.value:
+        switch(operation.operator) {
+            case TokenEnum.LESS_THAN.final:
+                return leftHandValue < rightHandValue;
+            case TokenEnum.GREATER_THAN.final:
+                return leftHandValue > rightHandValue;
+            case TokenEnum.EQUAL.final:
+                return leftHandValue == rightHandValue;
+            case TokenEnum.NOT_EQUAL.final:
+                return leftHandValue != rightHandValue;
+            case TokenEnum.GREATER_THAN_OR_EQUAL.final:
+                return leftHandValue >= rightHandValue;
+            case TokenEnum.LESS_THAN_OR_EQUAL.final:
+                return leftHandValue <= rightHandValue;
+        }
+    }
+
+    unaryEvaluation(operation) {
+        const rightHandValue = this.evaluate(operation.right);
+
+        switch(operation.operator) {
+            case TokenEnum.NOT.final:
                 return !rightHandValue;
-            case TokenEnum.MINUS.value:
+            case TokenEnum.MINUS.final:
                 return -rightHandValue;
             default:
                 throw new Error("Operation could not be parsed.");
