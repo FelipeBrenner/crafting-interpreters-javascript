03298beb602c03788dacc36162cb48491a8c9154
FelipeBrenner <felipeobrenner@hotmail.com>
2022-04-19 10:34:37 -0300

    feat: created structure with src folder

--- src/index.js
+++ src/index.js
@@ -8,19 +8,15 @@ const testExpression = "a = 3\nb = a < 4\n!b\nc = a - 2\nc";
 async function main() {
   const crawlCode = new CrawlCode(testExpression);
   const tokens = await crawlCode.crawl();
-  console.log("Tokens:");
-  console.log(tokens);
+  console.log("Tokens: \n" + tokens);
 
   const trees = [];
   for (const token of tokens) {
-    
     const crawlTokens = new CrawlTokens(token);
     const tree = await crawlTokens.crawl();
     trees.push(tree);
-
   }
-  console.log("Tree Object:");
-  console.log(trees);
+  console.log("Tree Object: \n" + trees);
   for (const tree of trees) {
     console.log("AST Tree: " + new TreePrinter(tree).print());
   }
