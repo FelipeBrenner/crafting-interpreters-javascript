2df24aa03b1c0bb3bd7b0dc0ac072b16333256b3
felipe ricardi <felipericardi5@gmail.com>
2022-04-21 11:41:55 -0300

    feat: adding chalk to improve readability of output

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import chalk from "chalk";
 import fs from "fs";
 import { CrawlCode } from "./CrawlCode.js";
 import { CrawlTokens } from "./CrawlTokens.js";
@@ -8,14 +9,18 @@ async function main() {
   let fileList = fs.readdirSync("scripts");
 
   for (const index in fileList) {
+
+    const startExecution = new Date();
+    console.log(chalk.bgCyan.black(`running ${fileList[index]}`))
     const code = fs.readFileSync("scripts/" + fileList[index], {
       encoding: "utf8",
     });
 
+    console.log(chalk.bgCyan.black("\n\nCode:"));
+    console.log(code);
     const crawlCode = new CrawlCode(code);
     const tokens = await crawlCode.crawl();
-    // console.log("Tokens: \n" + tokens);
-
+    
     const trees = [];
     for (const token of tokens) {
       const crawlTokens = new CrawlTokens(token);
@@ -23,14 +28,17 @@ async function main() {
       trees.push(tree);
     }
     // console.log("Tree Object: \n" + trees);
-    console.log("AST Tree:");
+    console.log(chalk.bgCyan.black("\n\nAST Tree:"));
     for (const tree of trees) {
       console.log(new TreePrinter(tree).print());
     }
-
-    console.log("Code: \n" + code);
+    console.log(chalk.bgCyan.black("\n\Evaluation:"));
     const evaluator = new EvaluateTree(trees);
     console.log(evaluator.init());
+    const endExecution = new Date()
+    console.log(chalk.bgRed.black.underline(`\nend of execution ${fileList[index]} in ${endExecution.getTime() - startExecution.getTime()}ms`))
+    
+    console.log(chalk.cyan("------------------------------------"))
   }
 }
 
